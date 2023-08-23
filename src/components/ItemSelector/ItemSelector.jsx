9a9f5f17ab868a12a1fae638540400f58d48322e
nkasmanoff <nkasmanoff@gmail.com>
2023-08-23 11:17:30 -0400

    rm log

--- src/components/ItemSelector/ItemSelector.jsx
+++ src/components/ItemSelector/ItemSelector.jsx
@@ -5,7 +5,6 @@ import Autocomplete from '@mui/material/Autocomplete';
 export default function ItemSelector({setSelection}) {
 
   const handleSetSelection = (event, value) => {
-    console.log(value)
     setSelection(value)
   }
 
